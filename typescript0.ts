22a787f79d90e97d69aea00ce204e4f3686ab930
MVakharia <mvakharia.bcm@gmail.com>
2025-03-29 21:45:21 +0000

    3 days' worth of studying on Codecademy and making stuff.

---
+++ typescript0.ts
@@ -0,0 +1,32 @@
+//'console.log' is used to print stuff to the console. 
+console.log('Hello World!');
+
+//The 'length' operator returns the length of a string object.
+console.log('2 + 6'.length);
+
+console.log('Hello World'.length);
+
+console.log('Michael Vakharia'.length);
+
+console.log('The number of characters in my full name is ' + ('Michael Vakharia'.length - 1));
+
+//'toUpperCase' converts a string to uppercase.
+console.log('Michael Vakharia'.toUpperCase());
+
+console.log('I\'m a Unity developer.'.toUpperCase());
+
+console.log('I\'m also a web developer in training.'.toUpperCase());
+
+console.log('I\'m not yelling, I\'m just converting the strings to uppercase.'.toUpperCase());
+
+//'trim' removes white space from the ends of a string. 
+console.log('               This string has a lot of white space at the ends. I had to trim it down.                   '.trim());
+
+console.log('                I don\'t normally write sentences with this much whitespace at the ends.                    '.trim());
+
+//There are lots of Math functions: floor is one of them. See Intellisense and the JS docs for more details. 
+console.log(Math.floor(13.69));
+
+//Same with Number functions: there's a bunch. See Intellisense and JS docs for more info. 
+console.log(Number.isInteger(2025.25252525));
+
---
+++ typescript1.ts
@@ -0,0 +1,16 @@
+var personName = "Alice";
+
+console.log(personName);
+
+var x = 10;
+
+console.log(x);
+
+//You can declare the same variable twice in JS. It looks like the second declaration overwrites the first. 
+var x = 20;
+
+console.log(x);
+
+let city = 'London';
+
+console.log(city);
