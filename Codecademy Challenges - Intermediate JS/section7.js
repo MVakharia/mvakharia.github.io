3891d623ca175372191fd3a0c5bbc3c621ed6938
MVakharia <mvakharia.bcm@gmail.com>
2025-06-14 19:49:13 +0100

    Major update.
    Removed JavaScript files, split Study Topics HTML page into multiple webpages with links to and from the main Study Topics index, and removed styling.

--- Codecademy Challenges - Intermediate JS/section7.js
+++
@@ -1,9 +0,0 @@
-const numbers = [2, 7, 9, 171, 52, 33, 14]
-
-const toSquare = num => num * num;
-
-function squareNums (arr) {
-    return arr.map(toSquare);
-}
-
-console.log(squareNums(numbers));
